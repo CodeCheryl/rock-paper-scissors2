31ca56727c2009aea4df9a1358aa0ab1a5a64fb6
codeCheryl <cheryl.morgenstern@gmail.com>
2020-10-15 15:38:06 -0400

    Added "Play Again" button

---
+++ rps.js
@@ -0,0 +1,46 @@
+function robot() {
+    let player1 = document.getElementById("player1").value;
+    let arr = ["rock", "paper", "scissors"];
+  
+    player2.innerHTML = arr[Math.floor(Math.random() * arr.length)];
+    if (player1 === "") {
+      alert("You must choose first");
+      return (player2.innerHTML = "Click to see what the robot chose");
+    }
+  }
+  
+  function rockPaperScissors(rock, paper, scissors) {
+    let player1 = document.getElementById("player1").value;
+    let player2 = document.getElementById("player2").innerHTML;
+  
+    if (
+      (player1 === "rock" && player2 === "paper") ||
+      (player1 === "paper" && player2 === "scissors") ||
+      (player1 === "scissors" && player2 === "rock")
+    ) {
+      alert("THE ROBOT WON!");
+      return;
+    } else if (
+      (player2 === "rock" && player1 === "paper") ||
+      (player2 === "paper" && player1 === "scissors") ||
+      (player2 === "scissors" && player1 === "rock")
+    ) {
+      alert("YOU WON!");
+      return;
+    } else if (
+      (player1 === "rock" && player2 === "rock") ||
+      (player1 === "paper" && player2 === "paper") ||
+      (player1 === "scissors" && player2 === "scissors")
+    ) {
+      alert("Tie Game");
+      return;
+    }
+  
+    alert("Please choose");
+}
+
+function reset() {
+  player1.value = "";
+  player2.innerHTML = "Click to see what the robot chose"
+  return;
+}
